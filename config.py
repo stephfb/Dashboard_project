09b3004747dd28bdbd1a550538dc6ab19044d201
stephfb <83563978+stephfb@users.noreply.github.com>
2023-07-14 12:07:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
 
 
-#steph keys
 
-    # okx_api_key= 'ce12ebc9b-a94b-4b6b-bf50-146c20ad343d'
-    # okx_api_secret = '35CC70F28CCFF72CF887E2DCDA59D9ACD'
-    # okx_passphrase = 'Ops1234!'
-
-    #binance
-    # binance_api_key= 'jK8ULUbrYE1169kOK2pHKsGX1EKqMRPbHlNTW8dMeSwAwjod73skhJAssys6rZSY'
-    # binance_api_secret = 'Ps4NFOfoMOv0Gjy1ABF88dsNCG5CMWiIhxzAcnNnGjKhlHP2WO4Z5uW6AZGhToxD'
